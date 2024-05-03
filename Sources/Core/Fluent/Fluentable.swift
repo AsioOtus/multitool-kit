79a1bec1a2d8f921fa566b1df63f7087cbe50cdd
Asio Otus <asio.otus.verus@icloud.com>
2024-05-03 16:10:05 +0300

    Update Fluentable

--- Sources/Core/Fluent/Fluentable.swift
+++ Sources/Core/Fluent/Fluentable.swift
@@ -2,49 +2,6 @@ public protocol Fluentable: UseFluentable, DoFluentable, MutateFluentable, SetFl
 
 
 
-public protocol UseFluentable { }
-
-public extension UseFluentable {
-	@discardableResult
-	func use (_ block: (Self) throws -> Void) rethrows -> Self {
-		try block(self)
-		return self
-	}
-
-	@discardableResult
-	func use (_ block: () throws -> Void) rethrows -> Self {
-		try block()
-		return self
-	}
-}
-
-
-
-public protocol DoFluentable { }
-
-public extension DoFluentable {
-	@discardableResult
-	func `do` (_ block: (inout Self) throws -> Void) rethrows -> Self {
-		var selfCopy = self
-		try block(&selfCopy)
-		return self
-	}
-}
-
-
-
-public protocol MutateFluentable { }
-
-public extension MutateFluentable {
-	@discardableResult
-	mutating func mutate (_ block: (inout Self) throws -> Void) rethrows -> Self {
-		try block(&self)
-		return self
-	}
-}
-
-
-
 public protocol SetFluentable { }
 
 public extension SetFluentable {
@@ -53,10 +10,28 @@ public extension SetFluentable {
 		try block(&selfCopy)
 		return selfCopy
 	}
+
+	func set <T> (_ keyPath: WritableKeyPath<Self, T>, _ value: T) -> Self {
+		var selfCopy = self
+		selfCopy[keyPath: keyPath] = value
+		return selfCopy
+	}
 }
 
 public extension SetFluentable where Self: AnyObject {
 	func set (_ block: (Self) throws -> Void) rethrows -> Self {
+		try block(self)
+		return self
+	}
+}
+
+
+
+public protocol UseFluentable { }
+
+public extension UseFluentable {
+	@discardableResult
+	func use (_ block: (Self) throws -> Void) rethrows -> Self {
 		try block(self)
 		return self
 	}
@@ -73,5 +48,42 @@ public extension LetFluentable {
 
 	func `let` <T> (_ block: () throws -> T) rethrows -> T {
 		try block()
+	}
+}
+
+
+
+public protocol DoFluentable { }
+
+public extension DoFluentable {
+	@discardableResult
+	func `do` (_ block: (inout Self) throws -> Void) rethrows -> Self {
+		var selfCopy = self
+		try block(&selfCopy)
+		return self
+	}
+
+	@discardableResult
+	func `do` (_ block: () throws -> Void) rethrows -> Self {
+		try block()
+		return self
+	}
+}
+
+
+
+public protocol MutateFluentable { }
+
+public extension MutateFluentable {
+	@discardableResult
+	mutating func mutate (_ block: (inout Self) throws -> Void) rethrows -> Self {
+		try block(&self)
+		return self
+	}
+
+	@discardableResult
+	mutating func mutate <T> (_ keyPath: WritableKeyPath<Self, T>, _ value: T) -> Self {
+		self[keyPath: keyPath] = value
+		return self
 	}
 }
